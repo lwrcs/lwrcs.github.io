47a824004261c754b7a9e7c1b021208a1e7271d5
lwrcs <harrisonkalman@gmail.com>
2024-03-18 01:19:06 -0700

    Fixed menus, progress on cleaning up

--- game/js/loader.js
+++
@@ -1,9 +0,0 @@
-document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        // Wait for the whole document to be ready
-        // Introduce a delay (e.g., 1000 milliseconds) before hiding the loader
-        setTimeout(function() {
-            document.getElementById("loader-wrapper").style.display = "none";
-        }, 500);
-    }
-};
---
+++ menu.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    const menuDiv = document.querySelector('.nav-wrap nav ul');
+    const dropMenuDiv = document.querySelector('ul.dropdown-menu')
+    const menuHTML = `
+        <li><a href="#about">About Me</a></li>
+        <li><a href="visuals.html">Visuals</a></li>
+        <li><a href="music.html">Music</a></li>
+    `;
+    menuDiv.innerHTML = menuHTML;
+    dropMenuDiv.innerHTML = menuHTML;
+}
