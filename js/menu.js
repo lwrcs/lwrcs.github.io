b094e7a642a682e33e368fbbb8b8ea9b74835e1f
lwrcs <harrisonkalman@gmail.com>
2024-04-07 13:44:08 -0700

    fixed url for pages

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,8 @@ window.onload = function() {
     const menuDiv = document.querySelector('.nav-wrap nav ul');
     const dropMenuDiv = document.querySelector('ul.dropdown-menu')
     const menuHTML = `
-        <li><a href="#about">About Me</a></li>
-        <li><a href="http://lwrcs.com/projects/">Projects</a></li>
+        <!--<li><a href="about/">About Me</a></li>-->
+        <li><a href="projects/">Projects</a></li>
         <!--<li><a href="music.html">Music</a></li>-->
     `;
     menuDiv.innerHTML = menuHTML;
